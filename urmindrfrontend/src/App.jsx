c5012495d2cca6829ddeaf52ae4338ed92cd9b03
neema owji <neemaowji@gmail.com>
2025-09-27 00:51:15 -0400

    added seperate landing and account pages

--- urmindrfrontend/src/App.jsx
+++ urmindrfrontend/src/App.jsx
@@ -1,11 +1,33 @@
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import Navbar from './components/Navbar';
+import Landing from './pages/Landing';
+import Account from './pages/Account';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const auth = getAuth();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Router>
+      <Navbar user={user} />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={user ? <Account user={user} /> : <Landing />} />
       </Routes>
     </Router>
   );
